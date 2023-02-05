0c95c64d2e2c446c2627ee77f0a78740a408e593
vikarchukk <101953785+vikarchukk@users.noreply.github.com>
2023-02-05 12:12:50 +0100

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,74 +1,3 @@
-// ---------- cursor ----------
-
-const cursor = document.querySelector('#cursor');
-const cursorAura = document.querySelector('#cursor-aura');
-const bodyTwo = document.querySelector('body');
-
-document.addEventListener('mousemove', (e) => {
-  cursor.style.cssText = cursorAura.style.cssText = "left: " + e.clientX + "px; top: " + e.clientY + "px;";
-});
-
-const a = document.querySelectorAll('a');
-const btn = document.querySelectorAll('button');
-const prItem = document.querySelectorAll('.portfolio__item');
-const input = document.querySelectorAll('input');
-const textarea = document.querySelector('textarea');
-
-for (let i = 0; i < a.length; i++) {
-  a[i].addEventListener('mouseover', () => {
-    cursor.classList.add('active-cursor')
-    cursorAura.classList.add('active-cursor')
-  })
-  a[i].addEventListener('mouseout', () => {
-    cursor.classList.remove('active-cursor')
-    cursorAura.classList.remove('active-cursor')
-  })
-};
-
-for (let i = 0; i < btn.length; i++) {
-  btn[i].addEventListener('mouseover', () => {
-    cursor.classList.add('active-cursor')
-    cursorAura.classList.add('active-cursor')
-  })
-  btn[i].addEventListener('mouseout', () => {
-    cursor.classList.remove('active-cursor')
-    cursorAura.classList.remove('active-cursor')
-  })
-};
-
-for (let i = 0; i < prItem.length; i++) {
-  prItem[i].addEventListener('mouseover', () => {
-    cursor.classList.add('active-cursor')
-    cursorAura.classList.add('active-cursor')
-  })
-  prItem[i].addEventListener('mouseout', () => {
-    cursor.classList.remove('active-cursor')
-    cursorAura.classList.remove('active-cursor')
-  })
-};
-
-for (let i = 0; i < input.length; i++) {
-  input[i].addEventListener('mouseover', () => {
-    cursor.classList.add('active-cursor')
-    cursorAura.classList.add('active-cursor')
-  })
-  input[i].addEventListener('mouseout', () => {
-    cursor.classList.remove('active-cursor')
-    cursorAura.classList.remove('active-cursor')
-  })
-};
-
-textarea.addEventListener('mouseover', () => {
-  cursor.classList.add('active-cursor')
-  cursorAura.classList.add('active-cursor')
-})
-textarea.addEventListener('mouseout', () => {
-  cursor.classList.remove('active-cursor')
-  cursorAura.classList.remove('active-cursor')
-})
-
-// ---------- /cursor ----------
-
 // ---------- aos ----------
 
 AOS.init({
@@ -153,15 +82,6 @@ if (nav && navBtn) {
   });
 };
 
-navBtn.addEventListener('mouseover', () => {
-  cursor.classList.add('active-cursor')
-  cursorAura.classList.add('active-cursor')
-})
-navBtn.addEventListener('mouseout', () => {
-  cursor.classList.remove('active-cursor')
-  cursorAura.classList.remove('active-cursor')
-})
-
 // ---------- /nav ----------
 
 // ---------- scroll ----------
@@ -196,3 +116,125 @@ anchors.forEach(anchor => {
 // });
 
 // ---------- /show more ----------
+
+// ---------- cursor ----------
+
+class BigCircle {
+  constructor() {
+    this.root = document.body
+    this.cursor = document.querySelector(".curzr")
+    this.circle = document.querySelector(".curzr .circle")
+    this.dot = document.querySelector(".curzr .dot")
+
+    this.pointerX = 0
+    this.pointerY = 0
+    this.cursorSize = 82
+
+    this.circleStyle = {
+      boxSizing: 'border-box',
+      position: 'fixed',
+      top: `${ this.cursorSize / -2 }px`,
+      left: `${ this.cursorSize / -2 }px`,
+      zIndex: '2147483647',
+      width: `${ this.cursorSize }px`,
+      height: `${ this.cursorSize }px`,
+      backgroundColor: '#fff0',
+      borderRadius: '50%',
+      transition: '500ms, transform 93ms',
+      userSelect: 'none',
+      pointerEvents: 'none'
+    }
+
+    this.dotStyle = {
+      boxSizing: 'border-box',
+      position: 'fixed',
+      zIndex: '2147483647',
+      width: '6px',
+      height: '6px',
+      backgroundColor: '#fffd',
+      borderRadius: '50%',
+      userSelect: 'none',
+      pointerEvents: 'none',
+      transition: '250ms, transform 69.75ms'
+    }
+
+    if (CSS.supports("backdrop-filter", "invert(1) grayscale(1)")) {
+      this.circleStyle.backdropFilter = 'invert(1) grayscale(1)'
+      this.circleStyle.backgroundColor = '#fff0'
+      this.dotStyle.backdropFilter = 'invert(1) grayscale(1)'
+      this.dotStyle.backgroundColor = '#fff0'
+    } else {
+      this.circleStyle.backgroundColor = '#000'
+      this.circleStyle.opacity = '0.75'
+      this.dotStyle.backgroundColor = '#fff'
+      this.dotStyle.opacity = '0.75'
+    }
+
+    this.init(this.circle, this.circleStyle)
+    this.init(this.dot, this.dotStyle)
+  }
+
+  init(el, style) {
+    Object.assign(el.style, style)
+    this.cursor.removeAttribute("hidden")
+    
+    document.body.style.cursor = 'none'
+    document.body.querySelectorAll("button, label, input, textarea, select, a").forEach((el) => {
+      el.style.cursor = 'inherit'
+    })
+  }
+
+  move(event) {
+    this.pointerX = event.pageX
+    this.pointerY = event.pageY + this.root.getBoundingClientRect().y
+  
+    this.circle.style.transform = `translate3d(${this.pointerX}px, ${this.pointerY}px, 0)`
+    this.dot.style.transform = `translate3d(calc(-50% + ${this.pointerX}px), calc(-50% + ${this.pointerY}px), 0)`
+
+    if (event.target.localName === 'button' || 
+        event.target.localName === 'a' || 
+        event.target.onclick !== null ||
+        event.target.className.includes('curzr-hover') ||
+        event.target.className.includes('nav__icon') ||
+        event.target.localName === 'span'){
+      this.hover()
+    }
+  }
+
+  hover() {
+    this.circle.style.transform += ` scale(1.5)`
+  }
+
+  click() {
+    this.circle.style.transform += ` scale(0.75)`
+    setTimeout(() => {
+      this.circle.style.transform = this.circle.style.transform.replace(` scale(0.75)`, '')
+    }, 35)
+  }
+
+  remove() {
+    this.circle.remove()
+    this.dot.remove()
+  }
+}
+
+if (navigator.userAgent.indexOf('Safari') !== -1 && navigator.userAgent.indexOf('Chrome') === -1) {
+  cursor.remove();
+}
+
+(() => {
+  const cursor = new BigCircle()
+  if(!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    document.onmousemove = function (event) {
+      cursor.move(event)
+    }
+    document.onclick = function () {
+      cursor.click()
+    }
+  } else {
+    cursor.remove()
+  }
+  
+})()
+
+// ---------- /cursor ----------
