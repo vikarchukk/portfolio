2e3f856cc734eec372166682dfdc4d6a9b774e15
vikarchukk <101953785+vikarchukk@users.noreply.github.com>
2023-02-05 13:02:17 +0100

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -84,25 +84,6 @@ if (nav && navBtn) {
 
 // ---------- /nav ----------
 
-// ---------- scroll ----------
-
-const anchors = document.querySelectorAll('a[href*="#"]');
-
-anchors.forEach(anchor => {
-  anchor.addEventListener('click', event => {
-    event.preventDefault();
-
-    const blockID = anchor.getAttribute('href').substring(1);
-
-    document.getElementById(blockID).scrollIntoView({
-      behavior: 'smooth',
-      block: 'start'
-    })
-  })
-});
-
-// ---------- /scroll ----------
-
 // ---------- show more ----------
 
 // const btnPortfolio = document.querySelector('.portfolio__btn');
@@ -238,3 +219,54 @@ if (navigator.userAgent.indexOf('Safari') !== -1 && navigator.userAgent.indexOf(
 })()
 
 // ---------- /cursor ----------
+
+// ---------- scroll ----------
+
+if (!document.querySelector("html").classList.contains('w-editor')){
+  const lenis = new Lenis({
+    duration: 1.2,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
+    direction: 'vertical', // vertical, horizontal
+    gestureDirection: 'vertical', // vertical, horizontal, both
+    smooth: true,
+    smoothTouch: false,
+    touchMultiplier: 2,
+    infinite: false,
+  })
+
+  // Get scroll value. This is just for testing purposes. Delete this if you're not using the scroll value for anything.
+  lenis.on('scroll', ({ scroll, limit, velocity, direction, progress }) => {
+    console.log({ scroll, limit, velocity, direction, progress })
+  })
+
+  function raf(time) {
+    lenis.raf(time)
+    requestAnimationFrame(raf)
+  }
+
+  // Grab all elements that have a "data-target" attribute
+  const scrollButtons = document.querySelectorAll('[data-target]');
+
+  // For each element, listen to a "click" event
+  scrollButtons.forEach(button => {
+    button.addEventListener('click', e => {
+      e.preventDefault();
+
+      // get the DOM element by the ID (data-target value)
+      var target = button.dataset.target,
+          $el = document.getElementById(target.replace('#', ''));
+
+      // Use lenis.scrollTo() to scroll the page to the right element
+      lenis.scrollTo($el, {
+        offset: 0, 
+        immediate: false,
+        duration: 3,
+        easing: (x) => (x < 0.5 ? 4 * x * x * x : 1 - Math.pow(-2 * x + 2, 3) / 2), // https://easings.net
+      });
+    });
+  });
+
+  requestAnimationFrame(raf)
+}
+
+// ---------- /scroll ----------
